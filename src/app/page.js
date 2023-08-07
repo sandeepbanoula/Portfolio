2759f8a8f23cbc5b081308ab94d6fbc20340ade9
Sandeep Banoula <sandeep.banoula@gmail.com>
2023-08-08 01:36:23 +0530

    Add Navbar

--- src/app/page.js
+++ src/app/page.js
@@ -2,8 +2,19 @@ import Image from 'next/image'
 
 export default function Home() {
   return (
-    <div>
-      Hello, my name is Sandeep Singh Banoula
+    <div className="grid grid-rows-5 px-8">
+      <nav className="grid grid-cols-[2fr_1fr] py-8">
+        <div className="logo">
+          SJavascript
+        </div>
+        <ul className="primary-menu inline-grid grid-cols-[repeat(4,_auto)] justify-items-end items-center">
+          <li><a href="">Home</a></li>
+          <li><a href="#projects">Projects</a></li>
+          <li><a href="#about-us">About Us</a></li>
+          <li className="border-2 border-green-500 p-2"><a href="#contact-me">Contact Me</a></li>
+        </ul>
+      </nav>
+
     </div>
   )
 }
